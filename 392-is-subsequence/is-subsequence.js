455ca543328567806cb32d7b99368ebf46f77d5a
Coderh0u <122144956+Coderh0u@users.noreply.github.com>
2023-09-22 13:11:56 +0800

    Time: 56 ms (40.26%) | Memory: 42 MB (55.89%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+   const sChars = s.split("");
+  const tChars = t.split("");
+  // default to true
+  let output = true;
+  // if s is a single character, check if it exists in t
+  if (s.length === 1 && tChars.indexOf(sChars[0]) === -1) {
+    output = false;
+  }
+  // if s is a string,
+  for (let i = 0; i < sChars.length; i++) {
+    const posA = tChars.indexOf(sChars[i]);
+     tChars.splice(0, posA + 1);
+    // check if character exists
+    if (posA === -1) {
+      output = false;
+    }
+
+  }
+  console.log(output);
+  return output;
+};
