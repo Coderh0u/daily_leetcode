c75ac862baf8607c1b5976139afbfc1bf3284f8a
Coderh0u <122144956+Coderh0u@users.noreply.github.com>
2023-10-04 23:55:01 +0800

    Time: 47 ms (78.41%) | Memory: 42 MB (40.34%) - LeetSync

---
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minStartValue = function (nums) {
+  if (!nums.filter((i) => i < 0).length) return 1;
+  let sum = 0;
+  let minSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sum < minSum) {
+      minSum = sum;
+    }
+  }
+  return minSum * -1 + 1;
+};
