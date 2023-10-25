1a3153e26f3b02ec2c6129ad9d74fad10fc38597
Coderh0u <122144956+Coderh0u@users.noreply.github.com>
2023-10-25 21:13:27 +0800

    Time: 88 ms (94.05%) | Memory: 63.6 MB (53.59%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -6,20 +6,18 @@ var longestConsecutive = function (nums) {
   if (nums.length === 0) return 0;
   const set1 = new Set(nums);
   const testArr = Array.from(set1);
-  const arr = [];
+  let maxCount = 0;
   for (let i = 0; i < testArr.length; i++) {
     let start;
     let seqLength = 0;
     if (!set1.has(testArr[i] - 1)) {
       start = testArr[i];
-      console.log(start);
       while (set1.has(start)) {
         seqLength++;
         start++;
       }
     }
-    arr.push(seqLength);
+    maxCount = Math.max(seqLength, maxCount);
   }
-  console.log(arr);
-  return Math.max(...arr);
+  return maxCount;
 };
