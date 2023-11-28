e033646e1db6265ce040a033330f5f3de2f331f2
Coderh0u <122144956+Coderh0u@users.noreply.github.com>
2023-11-29 00:20:50 +0800

    Time: 53 ms (47.97%) | Memory: 41.9 MB (54.88%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -5,22 +5,16 @@
 var findMin = function (nums) {
   let left = 0;
   let right = nums.length - 1;
-  let ans = 0;
+  let ans = nums[0];
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[mid + 1]) {
-      ans = nums[mid + 1];
-      break;
+    if (nums[mid] >= nums[0]) {
+      ans = Math.min(ans, nums[mid]);
+      left = mid + 1;
     } else {
-      if (nums[mid] < nums[mid + 1] && nums[mid] > nums[left]) {
-        left = mid;
-      } else if (nums[mid] < nums[mid + 1] && nums[mid] < nums[left]) {
-        right = mid;
-      } else {
-        ans = nums[0];
-        break;
-      }
+      ans = Math.min(ans, nums[mid]);
+      right = mid - 1;
     }
   }
   return ans;
