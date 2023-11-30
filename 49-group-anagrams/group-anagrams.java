16859667dc95a1e0a30297cf56db9d8ae86872bd
Coderh0u <122144956+Coderh0u@users.noreply.github.com>
2023-11-30 17:18:57 +0800

    Time: 6 ms (74.61%) | Memory: 46.7 MB (74.13%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,13 +1,22 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        
-    Map<String, List<String>> anagrams = new HashMap<>();
+  public List<List<String>> groupAnagrams(String[] strs) {
+    List<List<String>> output = new ArrayList<>();
+    HashMap<String, List<String>> anagrams = new HashMap<String, List<String>>();
     for (int i = 0; i < strs.length; i++) {
-      char[] charsInWord = strs[i].toCharArray();
-      Arrays.sort(charsInWord);
-      String sortedWord = new String(charsInWord);
-      anagrams.computeIfAbsent(sortedWord, k -> new ArrayList<>()).add(strs[i]);
+      char[] charArray = strs[i].toCharArray();
+      Arrays.sort(charArray);
+      String sortedStr = new String(charArray);
+      if (anagrams.containsKey(sortedStr)) {
+        anagrams.get(sortedStr).add(strs[i]);
+      } else {
+        List<String> temp = new ArrayList<>();
+        temp.add(strs[i]);
+        anagrams.put(sortedStr, temp);
+      }
     }
-    return new ArrayList<>(anagrams.values());
+    for (String asd : anagrams.keySet()) {
+      output.add(anagrams.get(asd));
     }
+    return output;
+  }
 }
